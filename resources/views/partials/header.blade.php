e3e988a755bfeb4d5f8dad1f76b0c943c73e2cca
Michael Hernandez <gammabeta010@gmail.com>
2020-05-07 10:47:50 +0200

    Agguinto partial files

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <img src="https://d33wubrfki0l68.cloudfront.net/3fbf6ef77e5e00aea7a22423087373169b52ae7e/413ff/images/logo/logo.svg" alt="">
+    <nav>
+        <ul>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Guides</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Memberships</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">More</a></li>
+        </ul>
+    </nav>
+    <button type="button" name="button" placeholder="Search"></button>
+</header>
